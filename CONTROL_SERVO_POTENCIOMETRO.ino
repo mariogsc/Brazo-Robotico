94109fd0a57a7a0eb546e88e170f7c16a47422f7
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-05-09 21:00:19 +0200

    Update CONTROL_SERVO_POTENCIOMETRO.ino

--- CONTROL_SERVO_POTENCIOMETRO.ino
+++ CONTROL_SERVO_POTENCIOMETRO.ino
@@ -3,18 +3,14 @@
 Servo myServo;
 int const PotPin=A0;
 int PotVal;
-int angle;
+
 void setup(){
-myServo.attach(8);
+
 Serial.begin(9600);
 }
 void loop(){
 PotVal=analogRead(PotPin);
-Serial.print("PotVal:");
+Serial.print("El Valor del potenciometro es: ");
 Serial.print(PotVal);
-angle=map(PotVal,0,300,0,180);
-Serial.print ("angle= ");
-Serial.println (angle);
-myServo.write(angle);
 delay (150);
 }
